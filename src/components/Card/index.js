36dcd5f4920147c48d06f9864166857c71454493
Sam Goldstein <sgoldstein@redfoundry.com>
2021-12-09 16:31:45 -0600

    Updated styling of card

---
+++ src/components/Card/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+
+const ContentCard = ({ title, src, body, onClick, footerText, style }) => (
+  <Col>
+    <Card style={{ width: "18rem", margin: "20px" }}>
+      <Card.Img variant="top" src={src} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{body}</Card.Text>
+        <Button onClick={onClick} variant="primary">
+          {footerText}
+        </Button>
+      </Card.Body>
+    </Card>
+  </Col>
+);
+
+export default ContentCard;
--- src/screens/Splash/Splash.js
+++ src/screens/Splash/Splash.js
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from "react";
 
 import axios from "axios";
+import { Link, Route } from "react-router-dom";
 
 import UserDropdown from "../../components/DropdownMenu";
 import DisplayButton from "../../components/Button";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
+import ContentCard from "../../components/Card";
+import ModalConfirmation from "../../components/Modal";
+
 import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 export default function Splash() {
   const [token, setToken] = useState();
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
   const [searchType, setSearchType] = useState("artists");
+  const [topData, setTopData] = useState();
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     if (localStorage.getItem("Access_Token")) {
       setToken(localStorage.getItem("Access_Token"));
-      console.log("TOKEN", token);
       setIsLoading(false);
       if (token) {
         axios
@@ -28,15 +32,16 @@ export default function Splash() {
             },
           })
           .then((response) => {
-            console.log("Response:", response.data);
             setData(response?.data);
             setIsLoading(false);
           })
           .catch((e) => {
             console.log("THIS IS THE ERROR", e);
-            localStorage.clear();
+            // localStorage.clear();
           });
       }
+    } else {
+      console.log("This will eventually be the logout");
     }
   }, [token]);
 
@@ -49,60 +54,77 @@ export default function Splash() {
       })
       .then((response) => {
         console.log("Top Response:", response.data);
-        // setData(response.data);
-        // setType(response.data.type);
-        // setIsLoading(false);
+        setTopData(response.data.items);
+        console.log("TOP DATA", topData);
       })
       .catch((e) => {
         console.log("THIS IS THE ERROR", e);
+        setShow(true);
       });
+  };
+
+  const signoutModal = () => {
+    return (
+      <>
+        <ModalConfirmation
+          title={"You are logged out"}
+          body={
+            "You have been logged out due to inactivity, please proceed to log back in."
+          }
+          buttonTitle={"Login"}
+          onClick={() => {
+            // <Link to="/login" />;
+            // console.log("CLICK");
+          }}
+          isOpen={show}
+        />
+      </>
+    );
   };
 
   return (
     <>
-      <div>
-        {isLoading ? (
-          <h1>Loading...</h1>
-        ) : (
-          <>
-            <UserDropdown
-              email={data?.email}
-              displayName={data?.display_name}
-              signout={"Sign Out"}
-              //TODO sign out and redirect
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <UserDropdown
+            email={data?.email}
+            displayName={data?.display_name}
+            signout={"Sign Out"}
+            //TODO sign out and redirect
+            onClick={() => {
+              localStorage.clear();
+              setToken();
+            }}
+          />
+          <DisplayButton
+            label={"Your Top Artists"}
+            onClick={() => {
+              setSearchType("artists");
+              handleSearch();
+            }}
+          />
+          <DisplayButton
+            label={"Your Top Tracks"}
+            onClick={() => {
+              setSearchType("tracks");
+              handleSearch();
+            }}
+          />
+        </>
+      )}
+
+      <Container>
+        <Row className="justify-content-md-center">
+          {topData?.map((item, index) => (
+            <ContentCard
+              key={index}
+              title={item?.name}
+              footerText={"Visit the artist"}
+              onClick={() => console.log("Clicked")}
             />
-            <DisplayButton
-              label={"Your Top Artists"}
-              onClick={() => {
-                setSearchType("artists");
-                handleSearch();
-              }}
-            />
-            <DisplayButton
-              label={"Your Top Tracks"}
-              onClick={() => {
-                setSearchType("tracks");
-                handleSearch();
-              }}
-            />
-          </>
-        )}
-      </div>
-      <Container flud>
-        <Row>
-          <Col>
-            {/* TODO Click on the card and use route params to grab data */}
-            <Card
-              style={{
-                width: "25%",
-                border: "solid green 1px",
-              }}
-            >
-              {/* Map over the array and pull out what we want (title and song), and place it into a card component */}
-              <Card.Title className="text-uppercase">Title</Card.Title>
-              <Card.Text>Song</Card.Text>
-            </Card>
-          </Col>
+          ))}
         </Row>
       </Container>
     </>
