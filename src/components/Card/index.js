828b3841a12aabcaa10599e56b3b41fcf44472ec
Sam Goldstein <sgoldstein@redfoundry.com>
2021-12-14 21:30:20 -0600

    style updates to card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,14 +4,41 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 
+import { Colors } from "../../colors";
+
 const ContentCard = ({ title, src, body, onClick, footerText, style }) => (
   <Col>
-    <Card style={{ width: "18rem", margin: "10px" }}>
-      <Card.Img variant="top" src={src} />
+    <Card
+      style={{
+        width: "15rem",
+        height: "15rem",
+        margin: "20px",
+        backgroundColor: Colors.lightGrey,
+      }}
+    >
+      <Card.Img
+        variant="top"
+        src={src}
+        style={{
+          width: "50%",
+          borderRadius: "50%",
+          padding: "10px",
+          maxHeight: "50%",
+        }}
+      />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{body}</Card.Text>
-        <Button onClick={onClick} variant="primary">
+        <Card.Title style={{ color: Colors.white, height: "50%" }}>
+          {title}
+        </Card.Title>
+        {/* <Card.Text style={style}>{body}</Card.Text> */}
+        <Button
+          onClick={onClick}
+          style={{
+            backgroundColor: Colors.spotifyGreen,
+            borderColor: Colors.spotifyGreen,
+          }}
+          variant="primary"
+        >
           {footerText}
         </Button>
       </Card.Body>
