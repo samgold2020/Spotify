bcc4c9f6a27292f7cbf22699489ecddac0c71ee2
Sam Goldstein <sgoldstein312@gmail.com>
2022-01-10 17:44:50 -0600

    Update routes & add Spotify logo to nav

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 
-import axios from "axios";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-import styles from "./styles";
-import UserDropdown from "../DropdownMenu";
-import SpinLoader from "../SpinLoader";
+import styles from './styles';
+import UserDropdown from '../DropdownMenu';
+import SpinLoader from '../SpinLoader';
+import SpotifyLogo from './SpotifyLogo.png';
 
 function NavBar() {
   const [userData, setUserData] = useState();
@@ -15,54 +16,58 @@ function NavBar() {
 
   function signOut() {
     localStorage.clear();
-    console.log(" logged out successfully" + window.status);
-    window.location.href = "http://localhost:3001/home";
-}
+    console.log(' logged out successfully' + window.status);
+    window.location.href = 'http://localhost:3000/login';
+  }
 
   useEffect(() => {
-    const token = localStorage.getItem("Access_Token");
-    if (token){
+    const token = localStorage.getItem('Access_Token');
+    if (token) {
       getUserData(token);
     } else {
-      console.log("No Token")
+      console.log('No Token');
     }
-}, []);
+  }, []);
 
-async function getUserData(token) {
-  try {
-    let res = await axios({
-      url: "https://api.spotify.com/v1/me",
-      method: "get",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (res.status === 200) {
-      console.log("SUCCESSFUL GET", res);
-      setUserData(res?.data);
-      setIsLoading(false)
+  async function getUserData(token) {
+    try {
+      let res = await axios({
+        url: 'https://api.spotify.com/v1/me',
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.status === 200) {
+        console.log('SUCCESSFUL GET', res.data);
+        setUserData(res?.data);
+        setIsLoading(false);
+      }
+      return userData;
+    } catch (err) {
+      console.log('Error', err);
     }
     return userData;
-  } catch (err) {
-    console.log("THIS IS THE ERROR");
   }
-  return userData;
-}
 
   return (
     <Navbar style={styles.navbar} expand="lg">
       <Container fluid>
         <Navbar.Brand style={styles.navbarTitle} href="/splash">
-          Spotify Data
+          <img
+            style={{ width: '65px', height: '65px', marginLeft: '10px' }}
+            src={SpotifyLogo}
+            alt="Spotify Logo"
+          />
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="navbarScroll"
-          style={{ backgroundColor: "white" }}
+          style={{ backgroundColor: 'white' }}
         />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
+            style={{ maxHeight: '100px' }}
             navbarScroll
           >
             <Nav.Link style={styles.navbarLinks} href="/topartists">
@@ -72,17 +77,17 @@ async function getUserData(token) {
               Top Tracks
             </Nav.Link>
           </Nav>
-          {isLoading ?(
-      <SpinLoader />
-    ) : (
-          <UserDropdown
-            email={userData?.email}
-            name={userData?.display_name}
-            src={userData?.images[0].url}
-            signout={"Logout"}
-            onClick={signOut}
-          />
-    )}
+          {isLoading ? (
+            <SpinLoader />
+          ) : (
+            <UserDropdown
+              email={userData?.email}
+              name={userData?.display_name}
+              src={userData?.images[0].url}
+              signout={'Logout'}
+              onClick={signOut}
+            />
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
