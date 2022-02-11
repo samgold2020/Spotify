5eda7f163244187d4cdd115cbf9f8f34657294b5
Sam Goldstein <sgoldstein312@gmail.com>
2022-02-11 15:35:25 -0600

    Code cleanup

--- src/screens/TopTracks/Track.js
+++ src/screens/TopTracks/Track.js
@@ -1,53 +1,53 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-import axios from "axios";
-import { useLocation } from "react-router-dom";
-import  Container  from 'react-bootstrap/Container';
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 
 import { Colors } from '../../colors';
 
 function Track() {
-    const [trackData, setTrackData] = useState();
+  const [trackData, setTrackData] = useState();
 
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        viewTrack(location.state.detail);
-        // getArtistsDetails(location.state.detail);
-      }, []);
+  useEffect(() => {
+    viewTrack(location.state.detail);
+    // getArtistsDetails(location.state.detail);
+  }, []);
 
-    const viewTrack = async (trackId) => {
-        const token = localStorage.getItem("Access_Token");
-    
-        try {
-          let res = await axios({
-            url: `https://api.spotify.com/v1/audio-features/${trackId}`,
-            method: "get",
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          if (res.status === 200) {
-            console.log("SUCCESSFUL GET", res?.data);
-            setTrackData(res?.data);
-            // setIsLoading(false);
-          }
-            // return artistData;
-        } catch (err) {
-          console.log("THIS IS THE ERROR");
-        }
-      };
+  const viewTrack = async trackId => {
+    const token = localStorage.getItem('Access_Token');
 
-    return (
-        <>
-        <Container
+    try {
+      let res = await axios({
+        url: `https://api.spotify.com/v1/audio-features/${trackId}`,
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.status === 200) {
+        setTrackData(res?.data);
+        // setIsLoading(false);
+      }
+      // return artistData;
+    } catch (err) {
+      console.log('THIS IS THE ERROR');
+    }
+  };
+
+  console.log('trackData', trackData.energy);
+  return (
+    <>
+      <Container
         fluid
-        style={{ backgroundColor: Colors.darkGrey, height: "100vh" }}
-            >
-            <h1>Track!</h1>
-        </Container>
-        </>
-    );  
+        style={{ backgroundColor: Colors.darkGrey, height: '100vh' }}
+      >
+        <h1>Track!</h1>
+      </Container>
+    </>
+  );
 }
 
 export default Track;
