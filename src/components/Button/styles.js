b0d4a880f643b82697dc39e6df79872a4a2871e5
Sam Goldstein <sgoldstein312@gmail.com>
2022-04-19 21:36:22 -0500

    Update button styling

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -5,14 +5,12 @@ const styles = {
     backgroundColor: hoverPrimary ? Colors.lightGrey : Colors.spotifyGreen,
     borderColor: Colors.spotifyGreen,
     color: hoverPrimary ? Colors.spotifyGreen : Colors.lightGrey,
-    borderColor: Colors.spotifyGreen,
     fontSize: '1.5em',
   }),
   buttonSecondary: hoverSecondary => ({
     backgroundColor: hoverSecondary ? Colors.spotifyGreen : Colors.lightGrey,
-    borderColor: Colors.lightGrey,
-    color: hoverSecondary ? Colors.lightGrey : Colors.spotifyGreen,
     borderColor: Colors.spotifyGreen,
+    color: hoverSecondary ? Colors.lightGrey : Colors.spotifyGreen,
     fontSize: '1.5em',
   }),
 };
