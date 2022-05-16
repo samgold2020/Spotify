baec9f27cee3b60f388ea7305907c18b6b58b70f
Sam Goldstein <sgoldstein312@gmail.com>
2022-05-16 16:31:14 -0500

    remove unused auth context

--- src/context/auth.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const AuthContext = createContext();
-
-//This is the hook we will use
-export function useAuth() {
-  //it will pull whatever data it finds in the AuthContext
-  return useContext(AuthContext);
-}
