bd98d524b19baa8bd0336db19bb2652648c54db8
Sam Goldstein <sgoldstein312@gmail.com>
2022-05-17 14:57:56 -0500

    Add additional logic

--- src/queryHelper.js
+++ src/queryHelper.js
@@ -1,70 +1,100 @@
 import axios from 'axios';
 
+const endpoint = process.env.REACT_APP_BASEURL;
+
+//TODO check for token before calls?
 export const getArtistByName = async (artistId, token) => {
-  console.log('Artist by name', artistId, token);
-  try {
-    let res = await axios({
-      url: `https://api.spotify.com/v1/artists/${artistId}`,
-      method: 'get',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (res.status === 200) {
-      return res?.data;
+  if (token) {
+    try {
+      let res = await axios({
+        url: `${endpoint}artists/${artistId}`,
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.status === 200) {
+        return res?.data;
+      }
+    } catch (e) {
+      console.log('Error', e);
     }
-  } catch (e) {
-    console.log('Error', e);
   }
 };
 
 export const getTopArtists = async token => {
-  try {
-    let res = await axios({
-      url: 'https://api.spotify.com/v1/me/top/artists',
-      method: 'get',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (res.status === 200) {
-      return res?.data.items;
+  if (token) {
+    try {
+      let res = await axios({
+        url: `${endpoint}me/top/artists`,
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.status === 200) {
+        return res?.data.items;
+      }
+    } catch (e) {
+      console.log('Error', e);
     }
-  } catch (err) {
-    console.log('Top Artists Error', err);
   }
 };
 
 export const getTrackAudioFeatures = async (trackId, token) => {
-  try {
-    let res = await axios({
-      url: `https://api.spotify.com/v1/audio-features/${trackId}`,
-      method: 'get',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (res.status === 200) {
-      return res.data;
+  if (token) {
+    try {
+      let res = await axios({
+        url: `${endpoint}audio-features/${trackId}`,
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.status === 200) {
+        return res.data;
+      }
+    } catch (e) {
+      console.log('Error', e);
     }
-  } catch (e) {
-    console.log('Error getting the track', e);
   }
 };
 
 export const getTopTracks = async token => {
-  try {
-    let res = await axios({
-      url: 'https://api.spotify.com/v1/me/top/tracks',
-      method: 'get',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (res.status === 200) {
-      return res.data;
+  if (token) {
+    try {
+      let res = await axios({
+        url: `${endpoint}me/top/tracks`,
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.status === 200) {
+        return res?.data;
+      }
+    } catch (e) {
+      console.log('Error', e);
     }
-  } catch (e) {
-    console.log('Error', e);
+  }
+};
+
+export const getCurrentUserData = async token => {
+  if (token) {
+    try {
+      let res = await axios({
+        url: `${endpoint}me`,
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.status === 200) {
+        return res?.data;
+      }
+    } catch (e) {
+      // signOut();
+      console.log('Error', e);
+    }
   }
 };
